bdb7a654e57149ef1d7e05d54f2de50e5919088a
miguerubsk <miguerubsk@gmail.com>
2019-12-02 16:40:26 +0100

    empezada la implementación

--- MallaRegular.h
+++ MallaRegular.h
@@ -16,14 +16,57 @@
 
 template <class T>
 class MallaRegular {
+private:
+	float xMin, yMin, xMax, yMax;
+	float tamCasillaX, tamCasillaY;
+	vector<vector<Casilla<T> > > mr;
+	Casilla<T> *obtenerCasilla(float x, float y);
 public:
     MallaRegular(float aXMin, float aYMin, float aXMax, float aYMax, int nDivX, int nDivY);
     T buscarCercano(float x, float y);
     bool fueraAmbito(float x, float y);
     unsigned maxElementosPorCelda();
     unsigned mediaElementosPorCelda();
+    void insertar(float x, float y, const T &dato);
+ 	T *buscar(float x, float y, const T &dato);
+ 	bool borrar(float x, float y, const T &dato);
+};
+
+template<typename Y>
+class Casilla{
+private:
+	list<Y> puntos;
+public:
+	friend class MallaRegular<Y>;
+	Casilla(): puntos() {}
+	void insertar(const Y &dato) { puntos.push_back(dato); }
+	Y *buscar(const Y &dato);
+	bool borrar(const Y &dato);
+};
+
+template<typename T>
+T *Casilla<T>::buscar(const T& dato) {
+	typename list<T>::iterator it;
+	it = puntos.begin();
+	for (it != puntos.end(); ++it){
+		if (*it ==dato)
+			return &(*it);
+	}
+	return 0;
 }
 
+template<typename T>
+bool Casilla<T>::borrar(const T& dato) {
+ 	typename list<T>::iterator it;
+ 	it = puntos.begin();
+ 	for (;it != puntos.end(); ++it){
+ 		if (*it == dato) {
+ 			puntos.erase(it);
+ 			return true;
+ 		}
+ 	}
+ 	return false;
+}
 
 #endif /* MALLAREGULAR_H */
 
